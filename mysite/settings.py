e9f527e1631beef07bbafba928d494fb32febc0f
subham-dhakal <subhamdhakal123@gmail.com>
2020-04-27 12:40:39 +0545

    Changed debug again

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['cool-blog-django.herokuapp.com','127.0.0.1']
 
